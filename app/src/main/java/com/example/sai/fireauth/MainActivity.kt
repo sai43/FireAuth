8c1da27001e867d78c53704ae511e9555c594c33
Sai Ch <csai@traveltripper.com>
2017-09-21 20:25:44 +0530

    setup kotlin extenstion and toast message for buttons

--- app/src/main/java/com/example/sai/fireauth/MainActivity.kt
+++ app/src/main/java/com/example/sai/fireauth/MainActivity.kt
@@ -2,11 +2,45 @@ package com.example.sai.fireauth
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onClick(p0: View?) {
+        when(p0){
+            btnLogin ->{
+                shortToast("Login")
+            }
+
+            btnRegister ->{
+                shortToast("Register")
+            }
+        }
+
+    }
+
+    private fun shortToast(message: String, length: Int = Toast.LENGTH_SHORT){
+        Toast.makeText(this, message, length).show()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+        initToolbar()
+    }
+
+    private fun init(){
+        btnLogin.setOnClickListener(this)
+        btnRegister.setOnClickListener(this)
+    }
+
+    private fun initToolbar(){
+        val toolbar = findViewById(R.id.toolbarMain) as Toolbar
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "Login/Register"
     }
 }
