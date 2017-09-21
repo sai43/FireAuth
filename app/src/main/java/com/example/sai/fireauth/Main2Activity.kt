5234c87e9a6d738a1e0aab3fa38187a14603b08f
Sai Ch <csai@traveltripper.com>
2017-09-21 23:31:42 +0530

    display current user email code added

--- app/src/main/java/com/example/sai/fireauth/Main2Activity.kt
+++ app/src/main/java/com/example/sai/fireauth/Main2Activity.kt
@@ -2,11 +2,39 @@ package com.example.sai.fireauth
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import kotlinx.android.synthetic.main.activity_main2.*
 
 class Main2Activity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+        mAuth = FirebaseAuth.getInstance()
     }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = mAuth.currentUser as FirebaseUser
+        updateUI(currentUser)
+    }
+
+    private fun updateUI(currentUser: FirebaseUser) {
+        if(currentUser != null){
+            val email = currentUser.email
+            displayEmail.text = email
+
+        }else{
+            shortToast("Current User is Null")
+        }
+    }
+
+    private fun shortToast(message: String, length: Int = Toast.LENGTH_SHORT){
+        Toast.makeText(this, message, length).show()
+    }
+
 }
